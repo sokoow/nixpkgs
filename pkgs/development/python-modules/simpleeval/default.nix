03a62b045b8af4ad985326a0581cadcedfaf60ea
Frederik Rietdijk <fridh@fridh.nl>
2019-12-10 19:07:42 +0100

    python: simpleeval: 0.9.8 -> 0.9.10

--- pkgs/development/python-modules/simpleeval/default.nix
+++ pkgs/development/python-modules/simpleeval/default.nix
@@ -2,10 +2,10 @@
 
 buildPythonPackage rec {
   pname = "simpleeval";
-  version = "0.9.8";
+  version = "0.9.10";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "00fzwbjg98lsnmfzmbgzg1k8q8iqbahcxjnnlhzhb44phrhcxql5";
+    sha256 = "692055488c2864637f6c2edb5fa48175978a2a07318009e7cf03c9790ca17bea";
   };
   meta = {
     homepage = https://github.com/danthedeckie/simpleeval;
